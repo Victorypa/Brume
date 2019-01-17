bd5d9c079bb818f276c2ad28a4d16bddbd02363e
ElijahCoding <cuiyeqing960904@gmail.com>
2019-01-17 22:23:20 +0300

    leaving fonts and images, go forward

--- webpack.config.js
+++ webpack.config.js
@@ -11,12 +11,17 @@ module.exports = {
     module: {
         rules: [
             {
-                test: /\.scss$/,
+                test: /\.(scss|sass|css)/,
                 use: ExtractTextPlugin.extract({
                     fallback: 'style-loader',
                     use: ['css-loader', 'sass-loader']
                 })
-            }
+            },
+            {
+                test: /\.(eot|woff|woff2|svg|ttf)([\?]?.*)$/,
+                use: ['file-loader']
+            },
+            { test: /\.(png|woff|woff2|eot|ttf|svg)$/, use: ['url-loader?limit=100000'] }
         ]
     },
     plugins: [
