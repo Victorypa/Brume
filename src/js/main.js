4a5dfa2e166c0305b3c3e1a50a8b8715d1430747
vika <victorypa1992@yandex.ru>
2019-02-15 00:26:48 +0300

    tabs-content js

--- src/js/main.js
+++ src/js/main.js
@@ -6,3 +6,23 @@ let menu = document.getElementById('menu')
                     nav.classList.toggle("show");
                     menu.classList.toggle("show");
                   })
+
+
+function openTab(evt, TabName) {
+  var i, tabcontent, tablinks;
+  tabcontent = document.getElementsByClassName("tabcontent");
+  for (i = 0; i < tabcontent.length; i++) {
+    tabcontent[i].style.display = "none";
+  }
+  tablinks = document.getElementsByClassName("services__link");
+  for (i = 0; i < tablinks.length; i++) {
+    tablinks[i].className = tablinks[i].className.replace(" active", "");
+  }
+  document.getElementById(TabName).style.display = "block";
+  evt.currentTarget.className += " active";
+}
+
+
+document.getElementById("defaultOpen").click();
+
+   
