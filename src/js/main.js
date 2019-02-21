68d8411d7d773b7520c17f9084b82dac46963dea
victorypa <victorypa1992@yandex.ru>
2019-02-21 19:03:35 +0300

    services tabs re

--- src/js/main.js
+++ src/js/main.js
@@ -8,19 +8,78 @@ let menu = document.getElementById('menu')
                   })
 
 
-function openTab(evt, TabName) {
-  let i, tabcontent, tablinks;
-  tabcontent = document.getElementsByClassName("tabcontent");
-  for (i = 0; i < tabcontent.length; i++) {
-    tabcontent[i].style.display = "none";
+// function openTab(evt, TabName) {
+//   let i, tabcontent, tablinks;
+//   tabcontent = document.getElementsByClassName("tabcontent");
+//   for (i = 0; i < tabcontent.length; i++) {
+//     tabcontent[i].style.display = "none";
+//   }
+//   tablinks = document.getElementsByClassName("portfolio__link");
+//   for (i = 0; i < tablinks.length; i++) {
+//     tablinks[i].className = tablinks[i].className.replace(" active", "");
+//   }
+//   document.getElementById(TabName).style.display = "block";
+//   evt.currentTarget.className += " active";
+// }
+//
+//
+// document.getElementById("defaultOpen").click();
+
+
+const tabsHeader = document.querySelectorAll('.tabs-header');
+const tabsContent = document.querySelectorAll('.tabs-content');
+const tabTitleClass = '.tab-title';
+
+// "Closest()" IE polyfill
+const closest = (el, selector) => {
+  const matchesSelector = el.matches || el.webkitMatchesSelector || el.mozMatchesSelector || el.msMatchesSelector;
+
+  while (el) {
+    if (matchesSelector.call(el, selector)) {
+      return el;
+    } else {
+      el = el.parentElement;
+    }
   }
-  tablinks = document.getElementsByClassName("portfolio__link");
-  for (i = 0; i < tablinks.length; i++) {
-    tablinks[i].className = tablinks[i].className.replace(" active", "");
-  }
-  document.getElementById(TabName).style.display = "block";
-  evt.currentTarget.className += " active";
+  return null;
 }
 
+const showContent = () => {
+	for (let i=0, child; child=tabsHeader[i]; i++) {
+		const tabTitles = child.children;
+		for (let i=0, tabTitle; tabTitle=tabTitles[i]; i++) {
+			const tabItems = closest(tabTitle, tabTitleClass).parentElement.nextElementSibling.children;
+			console.log(tabItems);
+			if (tabTitles[i].dataset.state == 'active') {
+				tabTitles[i].classList.add('is-showed');
+				[].forEach.call(tabItems, () => {
+					tabItems[i].classList.add('is-showed');
+				});
+			} else {
+				tabTitles[i].classList.remove('is-showed');
+				[].forEach.call(tabItems, () => {
+					tabItems[i].classList.remove('is-showed');
+				});
+			};
+		};
+	};
+};
+// Отображаем установленные по умолчанию табы при загрузке
+showContent();
 
-document.getElementById("defaultOpen").click();
+const activateTab = e => {
+	const target = closest(e.target, '.tab-title');
+	const tabItems = closest(target, '.tab-title').parentElement.nextElementSibling.children;
+	const targetSiblings = Array.prototype.filter.call(closest(target, '.tab-title').parentNode.children, (child) => child !== target);
+	targetSiblings.forEach(sib => {
+		sib.classList.remove('is-showed');
+		sib.setAttribute('data-state', 'hidden');
+	});
+	target.classList.add('is-showed');
+	target.setAttribute('data-state', 'active');
+	showContent();
+};
+
+[].forEach.call(tabsHeader, el => {
+    el.addEventListener('click', activateTab);
+});
